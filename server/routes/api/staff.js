81d606a2aa41bc4e2554d8a31f23b19e2942c976
Sahngwoo Kim <sak772@usask.ca>
2021-11-15 23:46:55 -0600

    implemented staff PUT and bug fix in POST

--- server/routes/api/staff.js
+++ server/routes/api/staff.js
@@ -3,6 +3,23 @@ const { getConnection, escape } = require("../../mysqlLib");
 
 const staffRouter = Router();
 
+/**
+ * Check if the given request body is valid for POST and PUT request
+ * @param body the body to check
+ * @returns {boolean} true if it is valid, false otherwise
+ */
+function isValidBody(body) {
+    return body.userId !== undefined &&
+        body.firstName !== undefined &&
+        body.lastName !== undefined &&
+        body.homeAddress !== undefined &&
+        body.homeAddress.street !== undefined &&
+        body.homeAddress.city !== undefined &&
+        body.homeAddress.province !== undefined &&
+        body.homeAddress.postalCode !== undefined &&
+        body.homeAddress.country !== undefined
+}
+
 staffRouter.get("/", (request, response) => {
     let sql = "SELECT * FROM Staff INNER JOIN Address ON Staff.userId=Address.userId";
 
@@ -59,15 +76,116 @@ staffRouter.get("/", (request, response) => {
 });
 
 staffRouter.post("/", (request, response) => {
+    if (!isValidBody(request.body)) {
+        response.status(400);
+        response.send("Request body doesn't contain required parameters");
+        return;
+    }
+
+    let userId = request.body.userId;
+    let firstName = request.body.firstName;
+    let lastName = request.body.lastName;
+    let middleName = request.body.middleName;
+    let homeAddress = request.body.homeAddress;
+
+    let street = homeAddress.street;
+    let city = homeAddress.city;
+    let province = homeAddress.province;
+    let postalCode = homeAddress.postalCode;
+    let country = homeAddress.country;
+
     getConnection((err, con) => {
+        if (err) {
+            response.status(500);
+            response.send("Server couldn't connect to the database");
+            return;
+        }
 
-    })
+        let sql = "INSERT INTO Staff (userId, firstName, lastName";
+        if (middleName === undefined) {
+            sql += ") VALUES (" + escape(userId) + ", " + escape(firstName) + ", " + escape(lastName) + ")";
+        } else {
+            sql += ", middleName) VALUES (" + escape(userId) + ", " + escape(firstName) + ", " +
+                escape(lastName) + ", " + escape(middleName) + ")";
+        }
 
-   response.send("Hello from staff POST");
+        sql += ";INSERT INTO Address (userId, street, city, province, postalCode, country) VALUES (" + escape(userId) +
+            ", " + escape(street) + ", " + escape(city) + ", " + escape(province) + ", " + escape( postalCode) + ", " +
+            escape(country) + ")";
+
+        sql += ";INSERT INTO AccountType (userId, type) VALUES (" + escape(userId) + ", \"Staff\")";
+
+        con.query(sql, err => {
+            if (err) {
+                throw err;
+            }
+
+            response.status(201);
+            response.send("Created");
+        });
+    });
 });
 
 staffRouter.put("/", (request, response) => {
-    response.send("Hello from staff PUT");
+    if (!isValidBody(request.body)) {
+        response.status(400);
+        response.send("Request body doesn't contain required parameters");
+        return;
+    }
+
+    let userId = request.body.userId;
+    let firstName = request.body.firstName;
+    let lastName = request.body.lastName;
+    let middleName = request.body.middleName;
+    let homeAddress = request.body.homeAddress;
+
+    let street = homeAddress.street;
+    let city = homeAddress.city;
+    let province = homeAddress.province;
+    let postalCode = homeAddress.postalCode;
+    let country = homeAddress.country;
+
+    getConnection((err, con) => {
+        if (err) {
+            response.status(500);
+            response.send("Server couldn't connect to the database");
+            return;
+        }
+
+        con.query("SELECT * FROM Staff WHERE userId=?", [userId], (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            // check if the given userId exists
+            if (result.length === 0) {
+                response.status(404);
+                response.send("Staff not found with the given userId \"" + userId + "\"");
+                return;
+            }
+
+            let sql = "UPDATE Staff SET firstName=" + escape(firstName) + ", lastName=" + escape(lastName);
+
+            if (middleName !== undefined) {
+                sql += ", middleName=" + escape(middleName);
+            }
+
+            sql += " WHERE userId=" + escape(userId);
+
+            sql += ";UPDATE Address SET street=" + escape(street) + ", city=" + escape(city) + ", province=" +
+                escape(province) + ", postalCode=" + escape(postalCode) + ", country=" + escape(country) +
+                "WHERE userId=" + escape(userId);
+
+            con.query(sql, err => {
+                if (err) {
+                    throw err;
+                }
+
+                response.status(200);
+                response.send("Updated");
+            });
+        });
+    });
 });
 
 staffRouter.delete("/", (request, response) => {
