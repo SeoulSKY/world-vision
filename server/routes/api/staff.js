22cffedc7e5679600c6bb21318353efb227a84e0
Sahngwoo Kim <sak772@usask.ca>
2021-11-15 22:07:53 -0600

    implemented staff GET

--- server/routes/api/staff.js
+++ server/routes/api/staff.js
@@ -12,43 +12,56 @@ staffRouter.get("/", (request, response) => {
     }
 
     getConnection((err, con) => {
+        if (err) {
+            response.status(500);
+            response.send("Server couldn't connect to the database");
+            return;
+        }
+
         con.query(sql, (err, result) => {
             if (err) {
                 throw err;
-            } else {
-                // convert the result to json format
-                let newResult = JSON.parse(JSON.stringify(result));
+            }
+            // convert the result to json format
+            let newResult = JSON.parse(JSON.stringify(result));
 
-                let array = [];
-                for (let i = 0; i < newResult.length; i++) {
-                    let json = {
-                        userId: newResult[i].userId,
-                        firstName: newResult[i].firstName,
-                        lastName: newResult[i].lastName,
-                        homeAddress: {
-                            street: newResult[i].street,
-                            city: newResult[i].city,
-                            province: newResult[i].province,
-                            postalCode: newResult[i].postalCode,
-                            country: newResult[i].country
-                        }
-                    };
+            if (newResult.length === 0) {
+                response.status(404);
+                response.send("Staff not found with the given userId \"" + userId + "\"");
+                return;
+            }
 
-                    if (newResult.middleName !== undefined) {
-                        json.middleName = newResult[i].middleName;
+            let array = [];
+            for (let i = 0; i < newResult.length; i++) {
+                let json = {
+                    userId: newResult[i].userId,
+                    firstName: newResult[i].firstName,
+                    lastName: newResult[i].lastName,
+                    homeAddress: {
+                        street: newResult[i].street,
+                        city: newResult[i].city,
+                        province: newResult[i].province,
+                        postalCode: newResult[i].postalCode,
+                        country: newResult[i].country
                     }
+                };
 
-                    array.push(json);
+                if (newResult.middleName !== undefined) {
+                    json.middleName = newResult[i].middleName;
                 }
 
-                response.json(array);
+                array.push(json);
             }
+
+            response.json(array);
         });
     });
 });
 
 staffRouter.post("/", (request, response) => {
+    getConnection((err, con) => {
 
+    })
 
    response.send("Hello from staff POST");
 });
