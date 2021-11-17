e667f52cab568bad1d90ec12abc24f68a8544b2b
Bathiya <bar407@usask.ca>
2021-11-17 01:51:46 -0600

    Can now save staff account input into json

--- web/src/components/staffDashboard.tsx
+++ web/src/components/staffDashboard.tsx
@@ -7,7 +7,8 @@ const StaffDashboard = () => {
     
 
     const onSubmit = (data: any)=> {
-        console.log(data)
+        // test that we can assess the user posted form data
+        console.log(data.userId + " " + data.firstName + " " +data.country)
     }
     return (
         
@@ -17,21 +18,44 @@ const StaffDashboard = () => {
             <p>Enter staff personal details</p>
 
             <form onSubmit={handleSubmit(onSubmit)}>
-            <input type ="text" placeholder="userId" {...register("userID")}/>
-            <input type ="text" placeholder="firstName" {...register("firstName")}/>
-            <input type ="text" placeholder="middleName" {...register("middleName")}/>
-            <input type ="text" placeholder="lastName" {...register("lastName")}/> 
+            <input type ="text" placeholder="userId" {...register("userId") } required />
+            <input type ="text" placeholder="firstName" {...register("firstName")} required/>
+            <input type ="text" placeholder="middleName" {...register("middleName")} required/>
+            <input type ="text" placeholder="lastName" {...register("lastName")}required /> 
             <br/>
             <p>Enter staff home address</p>
             <br/>
-            <input type ="text" placeholder="street" {...register("street")} />
-            <input type ="text" placeholder="city" {...register("city")} />
-            <input type ="text" placeholder="province" {...register("province")}/>
-            <input type ="text" placeholder="postalCode" {...register("postalCode")}/>
-            <input type ="text" placeholder="country" {...register("country")} />
+            <input type ="text" placeholder="street" {...register("street")} required/>
+            <input type ="text" placeholder="city" {...register("city")} required />
+            <input type ="text" placeholder="province" {...register("province")} required/>
+            <input type ="text" placeholder="postalCode" {...register("postalCode")}required />
+            <input type ="text" placeholder="country" {...register("country")} required />
             <br/>
             <input type="submit" />        
         </form>
+
+
+
+        <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>   
 
         </div>
     )
