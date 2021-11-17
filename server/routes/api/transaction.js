c0ad2d7f42b362342df7882575f4b9fd881cc1da
Sahngwoo Kim <sak772@usask.ca>
2021-11-17 14:07:20 -0600

    implemented donation endpoints

--- server/routes/api/transaction.js
+++ server/routes/api/transaction.js
@@ -3,15 +3,33 @@ const {escape, getConnection} = require("../../mysqlLib");
 
 /**
  * Charge the given amount
- * @param cardNumber the card number
- * @param expirationDate the expiration date of the card
- * @param cvv the cvv of the card
+ * @param customerUserId The customerUserId who is going to send transaction
+ * @param recipientUserId The recipient who is going to recieve the transaction
+ * @param card the card object containing number, expirationDate, and cvv
  * @param amount the amount to charge
  * @param callback the function to be called when everything is done
  */
-function sendTransaction(cardNumber, expirationDate, cvv, amount, callback) {
+function sendTransaction(customerUserId, recipientUserId, card, amount, callback) {
+
+    // send transaction
     console.log("We earned " + amount + " dollars!");
-    callback(null);
+
+    getConnection((err, con) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        // add transaction history
+        con.query("INSERT INTO Transaction (customerUserId, recipientUserId, amount) VALUES (?, ?, ?)",
+            [customerUserId, recipientUserId, amount], err => {
+            if (err) {
+                throw err;
+            }
+
+            callback(null);
+            });
+    });
 }
 
 const transactionRouter = Router();
