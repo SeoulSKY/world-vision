15f797d9b337e06ea29c3da181f795d107226eb8
ramtin <ramtin.mahdavifar@gmail.com>
2021-11-17 02:37:57 -0600

    first version of CustomerForm.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CustomerForm from "./CustomerForm";
 
 function App() {
 
@@ -16,23 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          {flaskMessage}
-        </p>
-      </header>
+      <CustomerForm/>
     </div>
   );
 }
