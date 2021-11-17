2cd23a24810242ff3b86494d8c699a9fcedeb13b
Bathiya <bar407@usask.ca>
2021-11-17 01:25:56 -0600

    Editted Staff Page to get info as json

--- web/src/components/staffDashboard.tsx
+++ web/src/components/staffDashboard.tsx
@@ -1,26 +1,34 @@
 import React from "react";
-
-
+import { useForm } from 'react-hook-form';
 
 const StaffDashboard = () => {
+   
+    const {register, handleSubmit} = useForm();
+    
+
+    const onSubmit = (data: any)=> {
+        console.log(data)
+    }
     return (
+        
         <div>
             <b>Create New Staff Account</b>
             <br/>
             <p>Enter staff personal details</p>
-            <form>
-            <input type ="text" placeholder="userId" name="userId" />
-            <input type ="text" placeholder="firstName" name="firstName" />
-            <input type ="text" placeholder="middleName" name="middleName" />
-            <input type ="text" placeholder="lastName" name="lastName" /> 
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <input type ="text" placeholder="userId" {...register("userID")}/>
+            <input type ="text" placeholder="firstName" {...register("firstName")}/>
+            <input type ="text" placeholder="middleName" {...register("middleName")}/>
+            <input type ="text" placeholder="lastName" {...register("lastName")}/> 
             <br/>
             <p>Enter staff home address</p>
             <br/>
-            <input type ="text" placeholder="street" name="street" />
-            <input type ="text" placeholder="city" name="city" />
-            <input type ="text" placeholder="province" name="province" />
-            <input type ="text" placeholder="postalCode" name="postalCode" />
-            <input type ="text" placeholder="country" name="country" />
+            <input type ="text" placeholder="street" {...register("street")} />
+            <input type ="text" placeholder="city" {...register("city")} />
+            <input type ="text" placeholder="province" {...register("province")}/>
+            <input type ="text" placeholder="postalCode" {...register("postalCode")}/>
+            <input type ="text" placeholder="country" {...register("country")} />
             <br/>
             <input type="submit" />        
         </form>
