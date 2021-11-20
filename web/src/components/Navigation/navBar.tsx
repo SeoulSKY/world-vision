ee7a720bc0245276f610ebf591d3bbf2f29ea6e7
Bathiya <bar407@usask.ca>
2021-11-20 17:07:12 -0600

    Error in navBar

--- web/src/components/Navigation/navBar.tsx
+++ web/src/components/Navigation/navBar.tsx
@@ -1,25 +1,53 @@
+// @ts-ignore
+// error with router dom
 import Navbar from 'react-bootstrap/Navbar';
 import Container from "react-bootstrap/Container";
 import {Nav} from "react-bootstrap";
 
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
+import StaffDashboard from "../StaffDashboard/staffDashboard";
+import HomePage from "../Pages/homePage";
+
+
 const NavBar = () => {
     return (
-        <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">World Vision</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Staff Dashboard</Nav.Link>
-                        <Nav.Link href="#features">Customer Dashboard</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="/sign-in">Sign In</Nav.Link>
-                        <Nav.Link href="/sign-up">Sign Up</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
-        )
-    }
-    export default NavBar;
+        <Router>
+            <>
+                <Navbar bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand as={Link} to = {"/home"}>World Vision</Navbar.Brand>
+                        <Nav className="me-auto">
+
+                            <Nav.Link as={Link} to = {"/staffDashboard"} >Staff Dashboard</Nav.Link>
+                            <Nav.Link as={Link} to = {"/customerDashboard"} >Customer Dashboard</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to = {"/signIn"} >Sign In</Nav.Link>
+                            <Nav.Link as={Link} to = {"/signUp"} >Sign Up</Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+            </>
+            <div>
+                <Routes>
+                    <Route path="/StaffDashboard">
+                        <StaffDashboard/>
+                    </Route>
+
+                    <Route path="/home">
+                        <HomePage />
+                    </Route>
+                </Routes>
+            </div>
+        </Router>
+    )
+}
+export default NavBar;
 
