8b931d47ed95d340dba0195df50dfab65975c359
Sahngwoo Kim <sak772@usask.ca>
2021-11-25 11:07:10 -0600

    partially fixed css issue

--- web/src/components/Pages/homePage/homePage.tsx
+++ web/src/components/Pages/homePage/homePage.tsx
@@ -11,9 +11,9 @@ import community_development from "../../../images/community-development-orange.
 class HomePage extends Component {
     render() {
         return (
-            <div className="container">
+            <div className="container-1">
                 <section>
-                    <h2 className="noDisplay">Main Content</h2>
+                    <h2 className="noDisplay-1">Main Content</h2>
                     <article>
                         <h1>About Us</h1>
                         <h2>Learn About Child Sponsorship</h2>
@@ -32,55 +32,55 @@ class HomePage extends Component {
                             children.</p>
                     </article>
                 </section>
-                <div className="row">
-                    <div className="columns">
-                        <p className="thumbnail_align"><img src={icon_education_white} alt=""
-                                                            className="thumbnail"/></p>
+                <div className="row-1">
+                    <div className="columns-1">
+                        <p className="thumbnail_align-1"><img src={icon_education_white} alt=""
+                                                              className="thumbnail-1"/></p>
                         <h4>EDUCATION</h4>
                         <p>Access to education, classroom repairs, school supplies and training for teachers and parents
                             provide a child with opportunities for a better future.</p>
                     </div>
-                    <div className="columns">
-                        <p className="thumbnail_align"><img src={icon_water} alt=""
-                                                            className="thumbnail"/></p>
+                    <div className="columns-1">
+                        <p className="thumbnail_align-1"><img src={icon_water} alt=""
+                                                              className="thumbnail-1"/></p>
                         <h4>SAFE WATER</h4>
                         <p>Access to safe water through well-drilling, pumps and training in hygiene and sanitation
                             decreases child mortality and frees up time for education instead of water collection.</p>
                     </div>
-                    <div className="columns">
-                        <p className="thumbnail_align"><img src={icon_food} alt=""
-                                                            className="thumbnail"/></p>
+                    <div className="columns-1">
+                        <p className="thumbnail_align-1"><img src={icon_food} alt=""
+                                                              className="thumbnail-1"/></p>
                         <h4>NUTRITIOUS FOOD</h4>
                         <p>Going beyond food distribution to a focus on creating sustainable food supplies through
                             agricultural programs and nutritional training ensures that children are well-nourished and
                             healthy.</p>
                     </div>
-                    <div className="columns">
-                        <p className="thumbnail_align"><img src={icon_health} alt=""
-                                                            className="thumbnail"/></p>
+                    <div className="columns-1">
+                        <p className="thumbnail_align-1"><img src={icon_health} alt=""
+                                                              className="thumbnail-1"/></p>
                         <h4>HEALTHCARE</h4>
                         <p>Good health is the foundation of a child’s life. Access to essential services, vaccinations,
                             medical clinics and training in disease prevention provide stronger health, for children and
                             their communities.</p>
                     </div>
-                    <div className="columns">
-                        <p className="thumbnail_align"><img src={icon_childProtection} alt=""
-                                                            className="thumbnail"/></p>
+                    <div className="columns-1">
+                        <p className="thumbnail_align-1"><img src={icon_childProtection} alt=""
+                                                              className="thumbnail-1"/></p>
                         <h4>CHILD PROTECTION</h4>
                         <p>We advocate for child rights and encourage children to speak up against abuse and injustice.
                             By educating families about child rights they are better equipped to keep their children
                             safe.</p>
                     </div>
-                    <div className="columns">
-                        <p className="thumbnail_align"><img src={icon_finance} alt=""
-                                                            className="thumbnail"/></p>
+                    <div className="columns-1">
+                        <p className="thumbnail_align-1"><img src={icon_finance} alt=""
+                                                              className="thumbnail-1"/></p>
                         <h4>ECONOMIC EMPOWERMENT</h4>
                         <p>Microloans, business training and a variety of trades and skills training enable families to
                             provide for their children’s needs, as well as helping them to build a better future.</p>
                     </div>
-                    <div className="columns">
-                        <p className="thumbnail_align"><img src={community_development} alt=""
-                                                            className="thumbnail"/></p>
+                    <div className="columns-1">
+                        <p className="thumbnail_align-1"><img src={community_development} alt=""
+                                                              className="thumbnail-1"/></p>
                         <h4>COMMUNITY LEADERSHIP</h4>
                         <p>We work with local leaders to strengthen their skills, build relationships, identify
                             community needs and solutions, and encourage networking with government groups.</p>
