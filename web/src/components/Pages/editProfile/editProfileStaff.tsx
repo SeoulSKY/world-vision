f93d4ceda2ec6805cb9e2300a0f8324b719fa32e
Bathiya <bar407@usask.ca>
2021-11-24 21:25:46 -0600

    Users can now edit their account info

--- web/src/components/Pages/editProfile/editProfileStaff.tsx
+++ web/src/components/Pages/editProfile/editProfileStaff.tsx
@@ -1,14 +1,28 @@
-import React, {useEffect, useState} from "react"
+import React, {useState} from "react"
 import {Form, Button, Card, Alert, Container} from "react-bootstrap"
 
 import { Link, useNavigate } from "react-router-dom"
 import {useAuth} from "../../../contexts/AuthContext ";
 
 export default function EditProfileStaff() {
-    const emailRef = React.useRef() as React.MutableRefObject<HTMLInputElement>
-    const passwordRef = React.useRef() as React.MutableRefObject<HTMLInputElement>
-    const passwordConfirmRef = React.useRef() as React.MutableRefObject<HTMLInputElement>
-    const { currentUser, updatePasswordCurrentUser, updateEmailCurrentUser } = useAuth()
+    const emailRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        passwordRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        passwordConfirmRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        firstNameRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        middleNameRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        lastNameRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        buildingNumberRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        streetNumberRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        postalCodeRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        countryRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        cityRef = React.useRef() as React.MutableRefObject<HTMLInputElement>,
+        provinceRef = React.useRef() as React.MutableRefObject<HTMLInputElement>
+
+
+
+
+
+        const { currentUser, updatePasswordCurrentUser, updateEmailCurrentUser, updateStaffInfo } = useAuth()
 
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
@@ -30,6 +44,15 @@ export default function EditProfileStaff() {
         if (passwordRef.current.value) {
             promises.push(updatePasswordCurrentUser(passwordRef.current.value))
         }
+        promises.push(updateStaffInfo(firstNameRef.current.value,
+            middleNameRef.current.value,
+            lastNameRef.current.value,
+            buildingNumberRef.current.value,
+            streetNumberRef.current.value,
+            postalCodeRef.current.value,
+            countryRef.current.value,
+            cityRef.current.value,
+            provinceRef.current.value))
 
         Promise.all(promises)
             .then(() => {
@@ -45,7 +68,6 @@ export default function EditProfileStaff() {
     }
 
     return (
-
         <Container className="d-flex align-items-center justify-content-center " style={{minHeight: "60vh"}}>
             <div className="w-100" style={{maxWidth: '1000px'}}>
                 <Card style={{minHeight: 350}}>
@@ -60,25 +82,107 @@ export default function EditProfileStaff() {
                                     ref={emailRef}
                                     required
                                     defaultValue={currentUser.email}
+                                    style={{textAlign: "center"}}
                                 />
                             </Form.Group>
+                            <br/>
                             <Form.Group id="password">
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control
                                     type="password"
                                     ref={passwordRef}
                                     placeholder="Leave blank to keep the same"
+                                    style={{textAlign: "center"}}
                                 />
                             </Form.Group>
+
+                            <br/>
+
                             <Form.Group id="password-confirm">
                                 <Form.Label>Password Confirmation</Form.Label>
                                 <Form.Control
                                     type="password"
                                     ref={passwordConfirmRef}
                                     placeholder="Leave blank to keep the same"
+                                    style={{textAlign: "center"}}
                                 />
                             </Form.Group>
+
                             <br/>
+
+                            <Form.Group id="firstName">
+                                <Form.Label> First Name</Form.Label>
+                                <Form.Control type="text" style={{textAlign: "center"}} ref={firstNameRef}
+                                              required/>
+                            </Form.Group>
+
+                            <br/>
+
+                            <Form.Group id="middleName">
+                                <Form.Label> Middle Name</Form.Label>
+                                <Form.Control type="text" placeholder="Middle name is optional"
+                                              style={{textAlign: "center"}} ref={middleNameRef}/>
+                            </Form.Group>
+
+                            <br/>
+
+
+                            <Form.Group id="lastName">
+                                <Form.Label> Last Name</Form.Label>
+                                <Form.Control type="text" style={{textAlign: "center"}} ref={lastNameRef}
+                                              required/>
+                            </Form.Group>
+
+                            <br/>
+                            <br/>
+
+                            <h3>Edit Address</h3>
+                            <br/>
+
+                            <Form.Group id="buildingNumber">
+                                <Form.Control type="text" placeholder="Building number"
+                                              style={{textAlign: "center"}} ref={buildingNumberRef} required/>
+                            </Form.Group>
+
+                            <br/>
+
+                            <br/>
+
+                            <Form.Group id="street">
+                                <Form.Control type="text" placeholder="Street" style={{textAlign: "center"}}
+                                              ref={streetNumberRef} required/>
+                            </Form.Group>
+
+                            <br/>
+
+                            <Form.Group id="city">
+                                <Form.Control type="text" placeholder="City" style={{textAlign: "center"}}
+                                              ref={cityRef} required/>
+                            </Form.Group>
+
+                            <br/>
+
+                            <Form.Group id="province">
+                                <Form.Control type="text" placeholder="Province" style={{textAlign: "center"}}
+                                              ref={provinceRef} required/>
+                            </Form.Group>
+
+                            <br/>
+
+                            <Form.Group id="postalCode">
+                                <Form.Control type="text" placeholder="Postal Code" style={{textAlign: "center"}}
+                                              ref={postalCodeRef} required/>
+                            </Form.Group>
+
+                            <br/>
+
+                            <Form.Group id="country">
+                                <Form.Control type="text" placeholder="Country" style={{textAlign: "center"}}
+                                              ref={countryRef} required/>
+                            </Form.Group>
+
+                            <br/>
+
                             <Button disabled={loading} style={{background: "#212529"}} className="w-100" type="submit" >
                                 Update
                             </Button>
--- web/src/contexts/AuthContext .js
+++ web/src/contexts/AuthContext .js
@@ -135,13 +135,99 @@ export function AuthProvider({children}) {
     }
 
     function updateEmailCurrentUser(email)     {
-        console.log(email)
+
         return updateEmail(currentUser,email)
     }
 
     function updatePasswordCurrentUser(password) {
-        console.log(password)
+
         return updatePassword(currentUser,password)
+    }
+
+    function updateStaffInfo(firstName, middleName, lastName, buildingNumber, streetNumber, postalCode, country, city, province) {
+        const data_staff = {
+            "userId": currentUser.uid,
+            "firstName": firstName,
+            "middleName": middleName,
+            "lastName": lastName,
+            "homeAddress": {
+                "buildingNumber": buildingNumber,
+                "street": streetNumber,
+                "city": city,
+                "province": province,
+                "postalCode": postalCode,
+                "country": country
+            }
+        };
+
+        // Put request using fetch with error handling
+       return fetch('http://localhost:5000/api/staff', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data_staff),
+        }).then(async response => {
+            const isJson = response.headers.get('content-type')?.includes('application/json');
+            const data = isJson && await response.json();
+
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response status
+                const error = (data && data.message) || response.status;
+                return Promise.reject(error);
+            }
+
+
+        })
+
+
+    }
+
+    function updateCustomerInfo(firstName, middleName, lastName, buildingNumber, streetNumber, postalCode, country, city, province, expirationDate, creditCardNumber, cvv) {
+        const data_customer = {
+            "userId": currentUser.uid,
+            "firstName": firstName,
+            "middleName": middleName,
+            "lastName": lastName,
+            "billingAddress": {
+                "buildingNumber": buildingNumber,
+                "street": streetNumber,
+                "city": city,
+                "province": province,
+                "postalCode": postalCode,
+                "country": country
+            },
+            "card": {
+                "number": creditCardNumber,
+                "expirationDate": expirationDate,
+                "cvv": cvv
+            }
+        };
+
+        // Put request using fetch with error handling
+        return fetch('http://localhost:5000/api/customer', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data_customer),
+        }).then(async response => {
+            const isJson = response.headers.get('content-type')?.includes('application/json');
+            const data = isJson && await response.json();
+
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response status
+                const error = (data && data.message) || response.status;
+                return Promise.reject(error);
+            }
+
+        })
+
+
+
+
     }
 
     useEffect(() => {
@@ -191,6 +277,8 @@ export function AuthProvider({children}) {
         updateEmailCurrentUser,
         updatePasswordCurrentUser,
         resetPassword,
+        updateStaffInfo,
+        updateCustomerInfo
 
     }
 
